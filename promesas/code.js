5b6971f6d8381642bec39f246b3811874e10fe08
Blo2SonNT <migustanoeto.2@gmail.com>
2023-08-18 19:51:38 -0500

    API rick and morty

--- promesas/code.js
+++ promesas/code.js
@@ -18,48 +18,95 @@
 
 
 
-let urlApi = "https://rickandmortyapi.com/api/character"
-fetch(urlApi)
-    .then((res) => res.json())
-    .then((data) => {
-        let divGrilla = document.querySelector("#grillaRM")
-        console.log(data)
+function consumoAPI(urlApi = "https://rickandmortyapi.com/api/character?page=1", tipo = 'normal') {
 
-        data.results.forEach((personaje) => {
-            let estadoPersonaje = personaje.status
-            let claseEstado = ""
-            switch (estadoPersonaje) {
-                case 'Alive':
-                    claseEstado = 'alive'
-                    break;
-                case 'Dead':
-                    claseEstado = 'dead'
-                    break;
-                default:
-                    claseEstado = 'unknown'
-                    break;
-            }
+    if (tipo != 'normal') {
+        let busquedaUsuario = document.querySelector('#busqueda').value
+        urlApi = `${urlApi}&name=${busquedaUsuario}`
+    }
 
-            divGrilla.innerHTML += `
-            
-            <div class="col">
-                <div class="card">
-                    <img src="${personaje.image}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">${personaje.name}</h5>
-                        <ul class="list-group">
-                            <li class="list-group-item">${personaje.species}</li>
-                            <li class="list-group-item">
-                                <div class="estadoPersonaje ${claseEstado}"></div>
-                                ${personaje.status}</li>
-                            <li class="list-group-item">${personaje.gender}</li>
-                        </ul>
+    fetch(urlApi)
+        .then((res) => res.json())
+        .then((data) => {
+            let divGrilla = document.querySelector("#grillaRM")
+            divGrilla.className = "row row-cols-1 row-cols-md-4 g-4"
+            console.log(data)
+            divGrilla.innerHTML = ""
+                // if ("error" in data == false) {
+            if (!data.hasOwnProperty("error")) {
+                data.results.forEach((personaje) => {
+                    let estadoPersonaje = personaje.status
+                    let claseEstado = ""
+                    switch (estadoPersonaje) {
+                        case 'Alive':
+                            claseEstado = 'alive'
+                            break;
+                        case 'Dead':
+                            claseEstado = 'dead'
+                            break;
+                        default:
+                            claseEstado = 'unknown'
+                            break;
+                    }
+
+                    divGrilla.innerHTML += `
+                
+                <div class="col">
+                    <div class="card">
+                        <img src="${personaje.image}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${personaje.name}</h5>
+                            <ul class="list-group">
+                                <li class="list-group-item">${personaje.species}</li>
+                                <li class="list-group-item">
+                                    <div class="estadoPersonaje ${claseEstado}"></div>
+                                    ${personaje.status}</li>
+                                <li class="list-group-item">${personaje.gender}</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-            </div>
-            `
-        });
+                `
+                });
 
-    }).catch(error => {
-        console.error("error al consumir la api")
-    })
+                let atributoDisabledPrev = ""
+                let visibilityPrev = ""
+                let visibilityNext = ""
+                let atributoDisabledNext = ""
+                if (data.info.prev == null) {
+                    atributoDisabledPrev = "disabled"
+                    visibilityPrev = "invisible"
+                }
+                if (data.info.next == null) {
+                    atributoDisabledNext = "disabled"
+                    visibilityNext = "invisible"
+                }
+
+                document.querySelector("#paginacion").innerHTML = `
+                <div class="my-5 pb-5 d-flex justify-content-between">
+                    <button class="btn btn-dark ${visibilityPrev}" ${atributoDisabledPrev} onclick="consumoAPI('${data.info.prev}')">Anterior</button>
+                    <button class="btn btn-dark ${visibilityNext}" ${atributoDisabledNext} onclick="consumoAPI('${data.info.next}')">Siguiente</button>
+                </div>
+            `
+            } else {
+                divGrilla.innerHTML = `
+                    <h3 class="text-danger">No hay resultados</h3>
+                    <img src="https://pa1.aminoapps.com/6375/c80fcfc9df554f63db6c1d459381cb24d94427a0_00.gif"> 
+                    `
+                document.querySelector("#paginacion").innerHTML = ""
+                divGrilla.className = "text-center"
+            }
+
+        }).catch(error => {
+            console.error("error al consumir la api")
+            console.log(error)
+        })
+}
+
+consumoAPI()
+
+// function prueba(param1 = "pepe") {
+//     console.log(param1)
+// }
+
+// prueba("fulano")
