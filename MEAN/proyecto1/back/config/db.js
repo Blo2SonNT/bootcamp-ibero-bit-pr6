d961dba9a8e6cded4ec81a8a4638d5f1c71cf5ef
Blo2SonNT <migustanoeto.2@gmail.com>
2023-09-06 21:55:26 -0500

    [+] NODE - Creacion API parte 2

---
+++ MEAN/proyecto1/back/config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+require('dotenv').config({ path: 'config.env' })
+
+
+const conectarDB = async() => {
+    try {
+        await mongoose.connect(process.env.DB_MONGO, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        console.log("Base de datos conectada")
+    } catch (error) {
+        console.log(error)
+        process.exit(1)
+    }
+}
+
+
+module.exports = conectarDB
--- MEAN/proyecto1/back/index.js
+++ MEAN/proyecto1/back/index.js
@@ -1,1 +1,30 @@
-console.log("Hola desde nodemon pepito")
+const express = require('express')
+const conectarDB = require('./config/db')
+
+const app = express()
+conectarDB()
+
+
+//rutas - endpoint
+
+
+app.get('/api', (req, res) => {
+    res.send('Visitando la api')
+})
+app.post('/api/llamado2', (req, res) => {
+    res.send('llamando la api con POST 1')
+})
+app.post('/api', (req, res) => {
+    res.send('llamando la api con POST 2')
+})
+app.put('/api', (req, res) => {
+    res.send('llamando la api con PUT')
+})
+app.delete('/api', (req, res) => {
+    res.send('llamando la api con DELETE')
+})
+
+
+app.listen(3000, () => {
+    console.log("La aplicación se esta ejecutando en http://localhost:3000")
+})
